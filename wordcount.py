feecfb75248dcb17d0d9420c52190e61f2e220fc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 12:25:03 -0800

    Edited function to use dict.iteritems(). Added comments.

--- wordcount.py
+++ wordcount.py
@@ -3,17 +3,18 @@ def word_count(file_name):
     word_counts = {}
 
     for line in text_file:
-        # split line by space, get rid of trailing white space
-        words = line.rstrip().split()
+        # get rid of trailing whitespace, get a list of words
+        words = line.rstrip().split(' ')
 
         for word in words:
+            # update value or store a new key value pair
             word_counts[word] = word_counts.get(word, 0) + 1
 
-    # loop over dictionary
-    for word_key in word_counts:
-    #     print key value pairs
-        print word_key, word_counts[word_key]
+    # get key value pairs from word_counts
+    for word_key, word_value in word_counts.iteritems():
+        # print key value pairs
+        print word_key, word_value
 
     text_file.close()
 
-word_count("test.txt")
+word_count("twain.txt")
