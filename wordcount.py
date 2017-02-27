68bafeec2e66ee612dfd89ffb4e02aa4d0c76fab
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 12:57:46 -0800

    Implemented remove_punctuation() to strip words of punctuation

--- wordcount.py
+++ wordcount.py
@@ -1,14 +1,25 @@
+import string
+
+
+def remove_punctuation(phrase):
+    clean_phrase = [char for char in phrase if char not in string.punctuation]
+
+    return ''.join(clean_phrase)
+
+# remove_punctuation("Hello?,.")
+
 def word_count(file_name):
     text_file = open(file_name)
     word_counts = {}
 
     for line in text_file:
         # get rid of trailing whitespace, get a list of words
-        words = line.rstrip().split(' ')
+        words = line.rstrip().split()
 
         for word in words:
+            word = remove_punctuation(word)
             # update value or store a new key value pair
-            word_counts[word] = word_counts.get(word, 0) + 1
+            word_counts[word.lower()] = word_counts.get(word.lower(), 0) + 1
 
     # get key value pairs from word_counts
     for word_key, word_value in word_counts.iteritems():
@@ -17,4 +28,5 @@ def word_count(file_name):
 
     text_file.close()
 
-word_count("twain.txt")
+word_count("test.txt")
+
